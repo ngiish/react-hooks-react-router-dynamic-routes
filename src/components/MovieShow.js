1de57c1790738e6c51f1685d52d977be8996c068
Isaac <govegeku00@gmail.com>
2023-11-13 22:12:29 +0300

    Changes to components

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-function MovieShow() {
+function MovieShow({ movies }) {
+    //call useParams to access the `params` from the url
+    const params = useParams()
+    console.log(params)
+
   return (
-    <div>MovieShow</div>
+    <div>
+        {/* And here we access the `movieId` stored in `params` to render 
+        information about the selected movie */}
+      <h3>{movies[params.movieId].title}</h3>
+    </div>
   )
 }
 
